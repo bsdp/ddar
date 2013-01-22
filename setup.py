dd3cb8fce6c7fd84a5cf3696e4230bb8c25ae3b9
David Anderson <danderson@d-xps.(none)>
2013-01-22 14:29:49 +0900

    Added new database design and migration script.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from setuptools import setup, Extension
 
 setup(name='ddar',
-      version='0.1',
+      version='0.2',
       author='Robie Basak',
       author_email='rb@synctus.com',
       url='http://www.synctus.com/ddar',
       packages=['synctus'],
-      scripts=['ddar'],
+      scripts=['ddar', 'ddar1to2'],
       ext_modules=[ Extension('synctus._dds', ['scan.c', 'rabin.c',
                                            'synctus/ddsmodule.c'],
                               include_dirs=['.'],
